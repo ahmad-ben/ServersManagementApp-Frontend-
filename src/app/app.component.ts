0da7256f42ecdbd2d35bd4764afd8ba9e38c2ed8
ahmad-ben <ahmadbenchakhti@gmail.com>
2023-10-23 21:17:44 +0100

    add the UI, and the ping functionality.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit, inject } from '@angular/core';
-import { Observable, catchError, map, of, startWith } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, map, of, startWith } from 'rxjs';
 import { AppDataStateEnum } from './enums/appStateData.enum';
+import { ServerStatusEnum } from './enums/serverStatus.enum';
 import { AppStateInterface } from './interfaces/appState.interface';
-import { CustomResponseInterface } from './interfaces/customResponse.interface';
+import { CustomResponseInterface, MultipleServersDataType, OneServerDataType } from './interfaces/customResponse.interface';
 import { ServerService } from './services/server.service';
 
 @Component({
@@ -14,14 +15,23 @@ import { ServerService } from './services/server.service';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
+  readonly appDataStateEnumVar = AppDataStateEnum;
+  readonly serverStatusEnumVar = ServerStatusEnum;
+
   appState$!: Observable<AppStateInterface<CustomResponseInterface>>;
+  private currentPingedIpSub = new BehaviorSubject<string>("");
+  currentPingedIpObs$ = this.currentPingedIpSub.asObservable();
+  private lastDataSub = new BehaviorSubject<CustomResponseInterface | null>(null);
 
   serverService = inject(ServerService);
 
   ngOnInit(){
+    console.log('The first enum to property', this.appDataStateEnumVar);
+    console.log('The first enum to property', this.serverStatusEnumVar);
     this.appState$ = this.serverService.servers$
       .pipe(
         map(response => {
+          this.lastDataSub.next(response);
           return {
             dataState: AppDataStateEnum.LOADED_STATE, appData: response
           }
@@ -35,6 +45,50 @@ export class AppComponent implements OnInit {
 
       )
   }
+
+  pingServer(ipAddress: string): void{
+    this.currentPingedIpSub.next(ipAddress);
+    this.appState$ = this.serverService.ping$(ipAddress)
+    .pipe(
+      map(response => {
+          (this.lastDataSub.value?.data as MultipleServersDataType).servers[
+            (this.lastDataSub.value?.data as MultipleServersDataType).servers.findIndex(
+              (server) => server.id ===
+              (response.data as OneServerDataType).server.id
+              )
+            ] = (response.data as OneServerDataType).server; //TODO: Clean this later
+          this.currentPingedIpSub.next('');
+          return {
+            dataState: AppDataStateEnum.LOADED_STATE,
+            appData: this.lastDataSub.value as CustomResponseInterface
+          }
+        }),
+
+        startWith({
+          dataState: AppDataStateEnum.LOADED_STATE,
+          appData: this.lastDataSub.value as CustomResponseInterface
+        }),
+
+        catchError((error: string) => {
+          this.currentPingedIpSub.next('');
+          return of({ dataState: AppDataStateEnum.ERROR_STATE, error })
+        })
+
+      )
+  }
+
+
+
+
+/*
+
+get the whole array with the previous values. search on the wanted server
+and extract it to change its value from the new response use the index
+in the previous array itself.
+after override the previous value, put it a
+
+
+*/
 
 }
 //========================================================================
--- src/app/enums/appStateData.enum.ts
+++ src/app/enums/appStateData.enum.ts
@@ -3,3 +3,39 @@ export enum AppDataStateEnum{
   LOADED_STATE = "LOADED_STATE",
   ERROR_STATE = "ERROR_STATE"
 }
+
+
+
+/*
+
+
+
+in angular I have this:
+  <tbody *ngFor="let server of appState.appData!.data.servers; let i = index" >
+and then I had this error:
+  Property 'servers' does not exist on type 'OneServerDataType |
+  MultipleServersDataType'. Property 'servers' does not exist on type
+  'OneServerDataType'.
+since:
+  export interface CustomResponseInterface {
+    timeStamp: Date;
+    statusCode: number;
+    status: string;
+    reason: string;
+    message: string;
+    developerMessage: string;
+    data: OneServerDataType | MultipleServersDataType
+  }
+
+  export interface OneServerDataType {
+    server: ServerDataType
+  }
+
+  export interface MultipleServersDataType {
+    servers: ServerDataType[]
+  }
+but i'm sure in this case that the type of the  is   so the property servers exist
+
+
+
+*/
